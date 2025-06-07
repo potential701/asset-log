dabdcbfbf7f90c756770156021bf129c3d8f400d
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-07 23:16:19 +0100

    Add AssetType, AssetStatus, AssetCondition enums to enums.ts

--- lib/enums.ts
+++ lib/enums.ts
@@ -2,3 +2,23 @@ export enum Role {
   USER = "user",
   ADMIN = "admin",
 }
+
+export enum AssetType {
+  SCANNER = "scanner",
+  RADIO = "radio",
+  LAPTOP = "laptop",
+  OTHER = "other",
+}
+
+export enum AssetStatus {
+  AVAILABLE = "available",
+  BUSY = "busy",
+  MAINTENANCE = "maintenance",
+  DECOMMISSIONED = "decommissioned",
+}
+
+export enum AssetCondition {
+  GOOD = "good",
+  DAMAGED = "damaged",
+  BROKEN = "broken",
+}
