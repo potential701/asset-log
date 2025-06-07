f928e93a0bf1101fc2bec6c837559b7a9761e497
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-07 16:25:45 +0100

    Change middleware.ts to redirect to /auth/login when not authenticated

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export default async function middleware(req: NextRequest) {
 
   const session = await getSession();
   if (!isPublicRoute && !session) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+    return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
 
   return NextResponse.next();
