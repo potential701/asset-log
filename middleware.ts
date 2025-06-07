a161271d265707733f5ff7a688e51616dc37f4d7
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-07 15:34:12 +0100

    Add middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@/lib/session";
+
+const publicRoutes = ["/login", "/signup"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const session = await getSession();
+  if (!isPublicRoute && !session) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
