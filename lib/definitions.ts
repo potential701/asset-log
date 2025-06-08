8701eab79222d90db3753404e7f7049f55556666
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-08 08:58:40 +0100

    Add CreateAssetForm definitions in definitions.ts

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
-import { Role } from "@/lib/enums";
+import { AssetStatus, AssetType, Role } from "@/lib/enums";
 
 export const SignupFormSchema = z.object({
   name: z.string().min(3, { message: "Must contain at least 3 characters" }).trim(),
   password: z
     .string()
-    .min(8, { message: "Must be at least 8 characters long" })
+    .min(8, { message: "Must contain at least 8 characters" })
     .regex(/[a-zA-Z]/, { message: "Must contain at least one letter" })
     .regex(/[0-9]/, { message: "Must contain at least one number" })
     .regex(/[^a-zA-Z0-9]/, { message: "Must contain at least one special character" })
@@ -35,6 +35,26 @@ export type LoginFormState =
       errors?: {
         name?: string[];
         password?: string[];
+      };
+      message?: string;
+      success?: boolean;
+    }
+  | undefined;
+
+export const CreateAssetFormSchema = z.object({
+  name: z.string().min(3, { message: "Must contain at least 3 characters" }).trim(),
+  type: z.nativeEnum(AssetType),
+  serial_number: z.string().min(6, { message: "Must contain at least 6 characters" }).trim(),
+  status: z.nativeEnum(AssetStatus),
+});
+
+export type CreateAssetFormState =
+  | {
+      errors?: {
+        name?: string[];
+        type?: string[];
+        serial_number?: string[];
+        status?: string[];
       };
       message?: string;
       success?: boolean;
