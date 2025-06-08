f2082d24691a879c859708f5e7f3c7949ef234f7
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-08 11:14:17 +0100

    Add check-out-button.tsx

---
+++ app/(index)/asset/_components/check-out-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useActionState, useEffect } from "react";
+import { checkOut } from "@/app/(index)/asset/_actions/asset-actions";
+import Form from "next/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function CheckOutButton({ userId, assetId }: { userId: number; assetId: number }) {
+  const [state, action, pending] = useActionState(checkOut, undefined);
+
+  useEffect(() => {
+    if (state?.message && !state.success) {
+      toast.error(state.message);
+    }
+
+    if (state?.success) {
+      toast.success(state.message);
+    }
+  }, [state]);
+
+  return (
+    <>
+      <Form action={action}>
+        <Input defaultValue={userId} name="user_id" className="hidden" />
+        <Input defaultValue={assetId} name="asset_id" className="hidden" />
+        <Button type="submit" outline disabled={pending} className="w-full">
+          {pending ? "Checking out..." : "Check out"}
+        </Button>
+      </Form>
+    </>
+  );
+}
