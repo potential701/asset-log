5e604f7c7fada8d4715d50a0233da1a3dd3c4997
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-08 08:58:53 +0100

    Add navigation.tsx

---
+++ app/(index)/layout.tsx
@@ -0,0 +1,18 @@
+import { SidebarLayout as CatalystSidebarLayout } from "@/components/ui/sidebar-layout";
+import { getSession } from "@/lib/session";
+import { getInitials } from "@/lib/string";
+import { Navbar, Sidebar } from "@/app/(index)/_components/navigation";
+
+export default async function SidebarLayout({ children }: { children: React.ReactNode }) {
+  const session = await getSession();
+  const initials = getInitials(session?.name ?? "Unidentified");
+
+  return (
+    <CatalystSidebarLayout
+      navbar={<Navbar initials={initials}></Navbar>}
+      sidebar={<Sidebar initials={initials} name={session?.name}></Sidebar>}
+    >
+      {children}
+    </CatalystSidebarLayout>
+  );
+}
