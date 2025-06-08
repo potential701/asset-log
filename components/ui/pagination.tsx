feba3132d071d1359befa7400a23d63792f65138
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-08 17:00:24 +0100

    Wrap PaginationPage with Link

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,22 +1,23 @@
-import clsx from 'clsx'
-import type React from 'react'
-import { Button } from './button'
+import clsx from "clsx";
+import type React from "react";
+import { Button } from "./button";
+import { Link } from "@/components/ui/link";
 
 export function Pagination({
-  'aria-label': ariaLabel = 'Page navigation',
+  "aria-label": ariaLabel = "Page navigation",
   className,
   ...props
-}: React.ComponentPropsWithoutRef<'nav'>) {
-  return <nav aria-label={ariaLabel} {...props} className={clsx(className, 'flex gap-x-2')} />
+}: React.ComponentPropsWithoutRef<"nav">) {
+  return <nav aria-label={ariaLabel} {...props} className={clsx(className, "flex gap-x-2")} />;
 }
 
 export function PaginationPrevious({
   href = null,
   className,
-  children = 'Previous',
+  children = "Previous",
 }: React.PropsWithChildren<{ href?: string | null; className?: string }>) {
   return (
-    <span className={clsx(className, 'grow basis-0')}>
+    <span className={clsx(className, "grow basis-0")}>
       <Button {...(href === null ? { disabled: true } : { href })} plain aria-label="Previous page">
         <svg className="stroke-current" data-slot="icon" viewBox="0 0 16 16" fill="none" aria-hidden="true">
           <path
@@ -29,16 +30,16 @@ export function PaginationPrevious({
         {children}
       </Button>
     </span>
-  )
+  );
 }
 
 export function PaginationNext({
   href = null,
   className,
-  children = 'Next',
+  children = "Next",
 }: React.PropsWithChildren<{ href?: string | null; className?: string }>) {
   return (
-    <span className={clsx(className, 'flex grow basis-0 justify-end')}>
+    <span className={clsx(className, "flex grow basis-0 justify-end")}>
       <Button {...(href === null ? { disabled: true } : { href })} plain aria-label="Next page">
         {children}
         <svg className="stroke-current" data-slot="icon" viewBox="0 0 16 16" fill="none" aria-hidden="true">
@@ -51,11 +52,11 @@ export function PaginationNext({
         </svg>
       </Button>
     </span>
-  )
+  );
 }
 
-export function PaginationList({ className, ...props }: React.ComponentPropsWithoutRef<'span'>) {
-  return <span {...props} className={clsx(className, 'hidden items-baseline gap-x-2 sm:flex')} />
+export function PaginationList({ className, ...props }: React.ComponentPropsWithoutRef<"span">) {
+  return <span {...props} className={clsx(className, "hidden items-baseline gap-x-2 sm:flex")} />;
 }
 
 export function PaginationPage({
@@ -65,34 +66,35 @@ export function PaginationPage({
   children,
 }: React.PropsWithChildren<{ href: string; className?: string; current?: boolean }>) {
   return (
-    <Button
-      href={href}
-      plain
-      aria-label={`Page ${children}`}
-      aria-current={current ? 'page' : undefined}
-      className={clsx(
-        className,
-        'min-w-9 before:absolute before:-inset-px before:rounded-lg',
-        current && 'before:bg-zinc-950/5 dark:before:bg-white/10'
-      )}
-    >
-      <span className="-mx-0.5">{children}</span>
-    </Button>
-  )
+    <Link href={href}>
+      <Button
+        plain
+        aria-label={`Page ${children}`}
+        aria-current={current ? "page" : undefined}
+        className={clsx(
+          className,
+          "min-w-9 before:absolute before:-inset-px before:rounded-lg",
+          current && "before:bg-zinc-950/5 dark:before:bg-white/10",
+        )}
+      >
+        <span className="-mx-0.5">{children}</span>
+      </Button>
+    </Link>
+  );
 }
 
 export function PaginationGap({
   className,
   children = <>&hellip;</>,
   ...props
-}: React.ComponentPropsWithoutRef<'span'>) {
+}: React.ComponentPropsWithoutRef<"span">) {
   return (
     <span
       aria-hidden="true"
       {...props}
-      className={clsx(className, 'w-9 text-center text-sm/6 font-semibold text-zinc-950 select-none dark:text-white')}
+      className={clsx(className, "w-9 text-center text-sm/6 font-semibold text-zinc-950 select-none dark:text-white")}
     >
       {children}
     </span>
-  )
+  );
 }
