f5db27b6df84adb0933919710b70f419a963629d
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-08 10:33:21 +0100

    Add a table for viewing assets in page.tsx

--- app/(index)/asset/page.tsx
+++ app/(index)/asset/page.tsx
@@ -1,20 +1,79 @@
 import { Heading } from "@/components/ui/heading";
 import { db } from "@/db/drizzle";
-import { asset } from "@/db/schema";
 import CreateAssetDialog from "@/app/(index)/asset/_components/create-asset-dialog";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { format, isAfter } from "date-fns";
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
-  const assets = await db.select().from(asset);
+  const assets = await db.query.asset.findMany({
+    with: {
+      issue: {
+        orderBy: (issue, { desc }) => [desc(issue.reported_at)],
+        limit: 1,
+      },
+      log: {
+        orderBy: (log, { desc }) => [desc(log.checked_out_at)],
+        limit: 1,
+        with: { user: { columns: { id: true, name: true } } },
+      },
+    },
+  });
 
   return (
-    <main className="">
+    <main className="grid grid-cols-1 gap-8">
       <Heading className="flex flex-row items-center justify-between">
         Assets
         <CreateAssetDialog />
       </Heading>
-      {assets.map((asset) => (
-        <div key={asset.id}>{asset.name}</div>
-      ))}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeader>Id</TableHeader>
+            <TableHeader>Name</TableHeader>
+            <TableHeader>Type</TableHeader>
+            <TableHeader>Status</TableHeader>
+            <TableHeader>Last Activity</TableHeader>
+            <TableHeader>Assigned To</TableHeader>
+            <TableHeader>Condition</TableHeader>
+            <TableHeader>Actions</TableHeader>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {assets.map((asset) => (
+            <TableRow key={asset.id}>
+              <TableCell>{asset.id}</TableCell>
+              <TableCell>{asset.name}</TableCell>
+              <TableCell>{asset.type}</TableCell>
+              <TableCell>{asset.status}</TableCell>
+              <TableCell>
+                {asset.log[0] === undefined
+                  ? "-"
+                  : isAfter(asset.log[0].checked_in_at ?? 0, asset.log[0].checked_out_at)
+                    ? `Returned: ${format(asset.log[0].checked_in_at!, "MMM do yyyy HH:mm")}`
+                    : `Checked out: ${format(asset.log[0].checked_out_at, "MMM do yyyy HH:mm")}`}
+              </TableCell>
+              <TableCell>
+                {asset.log[0] === undefined
+                  ? "-"
+                  : isAfter(asset.log[0].checked_in_at ?? 0, asset.log[0].checked_out_at)
+                    ? "-"
+                    : asset.log[0].user.name}
+              </TableCell>
+              <TableCell>{asset.log[0] === undefined ? "-" : asset.log[0].return_condition}</TableCell>
+              <TableCell className="max-w-24">
+                {asset.log[0] === undefined ? (
+                  <Button className="w-full">Check out</Button>
+                ) : (
+                  <Button className="w-full" disabled>
+                    Unavailable
+                  </Button>
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </main>
   );
 }
