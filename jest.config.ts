55ca41dc0906c76ce239036173d4fff7383432da
Viktors Makreckis <vmakreckis2809@gmail.com>
2025-06-11 18:46:27 +0100

    Fix jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-const nextJest = require('next/jest');
+import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -19,7 +19,7 @@ const customJestConfig = {
   ],
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
@@ -35,4 +35,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
